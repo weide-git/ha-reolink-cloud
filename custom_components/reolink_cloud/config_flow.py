e7b0128ac62717e260bcc7fa32b005cbc8eeede6
weide-git <109655757+weide-git@users.noreply.github.com>
2026-08-08 23:16:12 +0200

    Update config_flow.py

--- custom_components/reolink_cloud/config_flow.py
+++ custom_components/reolink_cloud/config_flow.py
@@ -17,34 +17,24 @@ class ReolinkCloudConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial setup step."""
-        errors = {}
 
         if user_input is not None:
-            username = user_input[CONF_USERNAME]
-            password = user_input[CONF_PASSWORD]
-
-            # Authentication will be implemented in the next step.
-            # For now we store the credentials and continue.
             return self.async_create_entry(
-                title=username,
+                title=user_input[CONF_USERNAME],
                 data={
-                    CONF_USERNAME: username,
-                    CONF_PASSWORD: password,
+                    CONF_USERNAME: user_input[CONF_USERNAME],
+                    CONF_PASSWORD: user_input[CONF_PASSWORD],
                 },
             )
 
         schema = vol.Schema(
             {
                 vol.Required(CONF_USERNAME): str,
-                vol.Required(CONF_PASSWORD): vol.All(
-                    str,
-                    vol.Length(min=1),
-                ),
+                vol.Required(CONF_PASSWORD): str,
             }
         )
 
         return self.async_show_form(
             step_id="user",
             data_schema=schema,
-            errors=errors,
         )
