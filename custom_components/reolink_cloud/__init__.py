e6c08ffd9b2cfffe1c0ceb78096b0f5ac990d71a
weide-git <109655757+weide-git@users.noreply.github.com>
2026-08-08 23:06:44 +0200

    Update __init__.py

--- custom_components/reolink_cloud/__init__.py
+++ custom_components/reolink_cloud/__init__.py
@@ -4,8 +4,12 @@ from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-DOMAIN = "reolink_cloud"
+from .const import DOMAIN
+from .coordinator import ReolinkCloudCoordinator
+
+PLATFORMS: list[str] = []
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
@@ -18,6 +22,19 @@ async def async_setup_entry(
     entry: ConfigEntry,
 ) -> bool:
     """Set up Reolink Cloud from a config entry."""
+
+    session = async_get_clientsession(hass)
+
+    coordinator = ReolinkCloudCoordinator(
+        hass,
+        session,
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
     return True
 
 
@@ -26,4 +43,7 @@ async def async_unload_entry(
     entry: ConfigEntry,
 ) -> bool:
     """Unload a Reolink Cloud config entry."""
+
+    hass.data[DOMAIN].pop(entry.entry_id, None)
+
     return True
