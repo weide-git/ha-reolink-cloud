0c77e70efb67836d290c1525c608e7fe27ffeb76
weide-git <109655757+weide-git@users.noreply.github.com>
2026-08-09 08:18:06 +0200

    Update const.py

--- custom_components/reolink_cloud/const.py
+++ custom_components/reolink_cloud/const.py
@@ -1,9 +1,9 @@
-"""Constants for the Reolink Cloud integration."""
+"""Constants for the Reolink Cloud / P2P integration."""
 
 DOMAIN = "reolink_cloud"
 
-CONF_TOKEN = "token"
+CONF_UID = "uid"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
 
 DEFAULT_SCAN_INTERVAL = 60
-
-CLOUD_API_URL = "https://apis.reolink.com"
