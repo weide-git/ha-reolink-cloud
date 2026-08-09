6e5f2bb1865d475c2294b98c971dcf07628d9e0f
weide-git <109655757+weide-git@users.noreply.github.com>
2026-08-09 13:49:34 +0200

    Update coordinator.py

--- custom_components/reolink_cloud/coordinator.py
+++ custom_components/reolink_cloud/coordinator.py
@@ -1,6 +1,6 @@
 """Data coordinator for Reolink P2P."""
 
-from **future** import annotations
+from __future__ import annotations
 
 import logging
 from datetime import timedelta
@@ -12,90 +12,122 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from .api import ReolinkCloudApi
 from .const import DEFAULT_SCAN_INTERVAL, DOMAIN
 
-_LOGGER = logging.getLogger(**name**)
+_LOGGER = logging.getLogger(__name__)
+
 
 class ReolinkCloudCoordinator(
-DataUpdateCoordinator[dict[str, Any]]
+    DataUpdateCoordinator[dict[str, Any]]
 ):
-"""Coordinate updates from the Reolink camera."""
+    """Coordinate updates from the Reolink camera."""
 
-```
-def __init__(
-    self,
-    hass: HomeAssistant,
-    uid: str,
-    username: str,
-    password: str,
-) -> None:
-    """Initialize the coordinator."""
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        uid: str,
+        username: str,
+        password: str,
+    ) -> None:
+        """Initialize the coordinator."""
 
-    self.api = ReolinkCloudApi(
-        uid=uid,
-        username=username,
-        password=password,
-    )
-
-    self._connected = False
-
-    super().__init__(
-        hass,
-        logger=_LOGGER,
-        name=DOMAIN,
-        update_interval=timedelta(
-            seconds=DEFAULT_SCAN_INTERVAL
-        ),
-    )
-
-@property
-def connected(self) -> bool:
-    """Return whether the camera is connected."""
-
-    return self._connected
-
-async def async_connect(self) -> bool:
-    """Connect to the camera in the executor."""
-
-    try:
-        data = await self.hass.async_add_executor_job(
-            self.api.connect
+        self.api = ReolinkCloudApi(
+            uid=uid,
+            username=username,
+            password=password,
         )
 
-        self._connected = bool(
-            data.get("connected", False)
+        self._connected = False
+        self._connecting = False
+
+        super().__init__(
+            hass,
+            logger=_LOGGER,
+            name=DOMAIN,
+            update_interval=timedelta(
+                seconds=DEFAULT_SCAN_INTERVAL
+            ),
         )
 
-        if self._connected:
-            _LOGGER.info(
-                "Reolink camera %s is connected",
-                self.api.uid,
-            )
+    @property
+    def connected(self) -> bool:
+        """Return whether the camera is connected."""
 
         return self._connected
 
-    except Exception:
-        self._connected = False
+    async def async_connect(self) -> bool:
+        """Connect to the camera in the executor."""
 
-        _LOGGER.exception(
-            "Failed to connect to Reolink camera %s",
-            self.api.uid,
-        )
+        if self._connecting:
+            _LOGGER.debug(
+                "Reolink camera %s connection attempt already running",
+                self.api.uid,
+            )
+            return self._connected
 
-        return False
+        self._connecting = True
 
-async def _async_update_data(
-    self,
-) -> dict[str, Any]:
-    """Update camera connection state."""
+        try:
+            _LOGGER.info(
+                "Starting background connection to Reolink camera %s",
+                self.api.uid,
+            )
 
-    if self._connected:
+            data = await self.hass.async_add_executor_job(
+                self.api.connect
+            )
+
+            self._connected = bool(
+                data.get("connected", False)
+            )
+
+            if self._connected:
+                _LOGGER.info(
+                    "Reolink camera %s is connected",
+                    self.api.uid,
+                )
+            else:
+                _LOGGER.warning(
+                    "Reolink camera %s connection returned disconnected",
+                    self.api.uid,
+                )
+
+            return self._connected
+
+        except Exception:
+            self._connected = False
+
+            _LOGGER.exception(
+                "Failed to connect to Reolink camera %s",
+                self.api.uid,
+            )
+
+            return False
+
+        finally:
+            self._connecting = False
+
+    async def _async_update_data(
+        self,
+    ) -> dict[str, Any]:
+        """Update camera connection state."""
+
+        if self._connected:
+            return {
+                "connected": True,
+                "uid": self.api.uid,
+            }
+
+        if not self._connecting:
+            _LOGGER.info(
+                "Reolink camera %s is not connected; "
+                "starting connection in background",
+                self.api.uid,
+            )
+
+            self.hass.async_create_task(
+                self.async_connect()
+            )
+
         return {
-            "connected": True,
+            "connected": False,
             "uid": self.api.uid,
         }
-
-    connected = await self.async_connect()
-
-    return {
-        "connected": connected,
-        "uid": self.api.uid,
-    }
