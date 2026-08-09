2066ec0566e5e60cfbded82862f3fe0fc89c54bc
weide-git <109655757+weide-git@users.noreply.github.com>
2026-08-09 07:42:06 +0200

    Update run.sh

--- reolink-p2p/run.sh
+++ reolink-p2p/run.sh
@@ -1,29 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "# REOLINK P2P RAW UDP TRANSPORT TEST"
 echo "RESULT: SHELL_START"
 echo "RESULT: CURRENT_DIR=$(pwd)"
-echo "RESULT: DATA_INHALT:"
-ls -la /data
 
 echo "RESULT: PYTHON_DATEI_IM_IMAGE:"
-ls -la /app/reolink_raw_udp_test.py
+ls -l /app/reolink_raw_udp_test.py
 
 echo "RESULT: OPTIONS_VORHANDEN"
+
 if [ -f /data/options.json ]; then
     echo "JA"
 else
     echo "NEIN"
+    echo "RESULT: SHELL_ENDE"
     exit 1
 fi
 
 echo "RESULT: PYTHON_START"
 
-python3 /app/reolink_raw_udp_test.py
-
-RC=$?
-
-echo "RESULT: PYTHON_EXITCODE=$RC"
-echo "RESULT: SHELL_ENDE"
-
-exit $RC
+exec python3 -u /app/reolink_raw_udp_test.py
